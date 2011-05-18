b960c1d273fdb0a753e79f0b351e70748d97c622
Nicholas Rasband <nrasband@Nicholas-Rasbands-iMac.local>
2011-05-18 15:59:54 -0600

    Commit 4. It's making me commit the mainController, even though I'm not using it and deleted it.

---
+++ SpanishandEnglishVocabApp/MainController.h
@@ -0,0 +1,14 @@
+//
+//  MainController.h
+//  SpanishandEnglishVocabApp
+//
+//  Created by Nicholas Rasband on 5/15/11.
+//  Copyright 2011 Nick Rasband. All rights reserved.
+//
+
+@interface MainController : NSObject 
+{
+    
+}
+
+@end
